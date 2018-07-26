49ac93e3a5ac9458d356455d5fd31d5c0262379c
Hans-Achim Muscate <hamuscate@gmx.net>
2018-07-26 14:01:02 +0000

    add DevOpsExercise Webpage

--- webroot/test.php
+++ webroot/test.php
@@ -4,4 +4,14 @@ $firstname = "Hans-Achim";
 $lastname = "Muscate";
 
 echo "Hello " . $firstname . " " . $lastname . "!";
+echo "<br>";
+echo $_SERVER['HTTP_USER_AGENT'];
+
+?>
+
+<form action="action.php" method="post">
+ <p>Your name: <input type="text" name="name" /></p>
+ <p>Your age: <input type="text" name="age" /></p>
+ <p><input type="submit" /></p>
+</form>
 
